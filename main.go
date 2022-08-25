dea9195b1db9f4c002e6e3969bfe20b4d039db66
David Santos <insa1988@gmail.com>
2022-08-24 22:54:58 -0500

    add getBook and json response

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Book struct {
-	ID     string `json:id`
+	ID     int    `json:id`
 	Title  string `json:title`
 	Author string `json:author`
 	Year   string `json:year`
@@ -18,11 +20,38 @@ var books []Book
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/books", getBooks)
+
+	books = append(books,
+		Book{
+			ID:     1,
+			Title:  "La culpa es de la vaca",
+			Author: "Varios Autores",
+			Year:   "2000-08-12",
+		},
+		Book{
+			ID:     2,
+			Title:  "El Marquéz de sade",
+			Author: "Donatien Alphonse ",
+			Year:   "1900-01-02",
+		},
+	)
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("test get mux")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(book)
+		}
+	}
 }
